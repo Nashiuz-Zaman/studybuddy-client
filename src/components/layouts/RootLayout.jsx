de4bfdab87b4a9d8494e2a97f73c1e776d0da9fc
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-06 01:41:41 +0600

    Update tailwind variables and add default font to the project

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <div>
+    <div className="font-default">
       <Outlet />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xsm": "360px",
+        xsm: "480px",
+        ltab: "850px",
+      },
+      spacing: {
+        sectionGapLg: "11rem",
+        sectionGapMd: "9rem",
+        sectionGapSm: "5rem",
+        elementGapSm: "1.4rem",
+        elementGapMd: "3rem",
+      },
+      colors: {
+        primary: "#3498db",
+        primaryLight: "#5dade2",
+        textPrimary: "#1C1B1B",
+        textMediumLight: "#1c1b1bcc",
+        textLight: "#1c1b1b99",
+        lightGray: "#eee",
+      },
+      fontFamily: {
+        default: "'Roboto', sans-serif",
+      },
+      borderRadius: {
+        default: "8px",
+      },
+    },
   },
   plugins: [],
 };
