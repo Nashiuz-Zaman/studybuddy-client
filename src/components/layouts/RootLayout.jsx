8818970adb75c1d8fe1a2da5cc03d7d4eddc3d1b
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-06 03:44:29 +0600

    Add the website logo to the header

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -4,10 +4,13 @@ import { Outlet } from "react-router-dom";
 // shared components
 import Header from "../shared/Header/Header";
 
+// data
+import brandLogo from "./../../assets/website-logo/logo.webp";
+
 function RootLayout() {
   return (
     <div className="font-default">
-      <Header />
+      <Header logo={brandLogo} />
       <Outlet />
     </div>
   );
