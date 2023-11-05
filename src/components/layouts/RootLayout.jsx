63aee4e25037162533db06e077743b65ded37049
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-06 03:14:46 +0600

    Place Header in the root layout

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -1,9 +1,13 @@
 // react-router imports
 import { Outlet } from "react-router-dom";
 
+// shared components
+import Header from "../shared/Header/Header";
+
 function RootLayout() {
   return (
     <div className="font-default">
+      <Header />
       <Outlet />
     </div>
   );
