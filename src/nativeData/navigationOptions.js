343b6472dcc971694b7b0047fd4d47b59f66b5ba
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-06 04:00:46 +0600

    Create navigation menu data

---
+++ src/nativeData/navigationOptions.js
@@ -0,0 +1,10 @@
+export const navOptionsAlways = [
+  { id: 0, text: "Home", url: "/" },
+  { id: 1, text: "Assigments", url: "/assignments" },
+];
+
+export const navOptionsLoggedIn = [
+  { id: 0, text: "My Assignments", url: "/my-assignments" },
+  { id: 1, text: "Create Assignments", url: "/create-assignments" },
+  { id: 2, text: "Submitted Assignments", url: "/submitted-assignments" },
+];
