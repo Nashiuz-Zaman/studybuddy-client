9390b1ce1cfed8ead652217de4131d061549307c
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-06 03:48:10 +0600

    Add button component to proect

---
+++ src/components/shared/ButtonBtn/ButtonBtn.jsx
@@ -0,0 +1,31 @@
+// react imports
+import PropTypes from "prop-types";
+
+const ButtonBtn = ({
+  text,
+  onClickFunction = null,
+  outlined = false,
+  modifyClasses = "",
+}) => {
+  return (
+    <button
+      onClick={onClickFunction}
+      className={`${
+        outlined
+          ? "bg-transparent border border-white text-white hover:bg-[rgba(255,255,255,0.5)]"
+          : "bg-primary border border-primary hover:border-primaryLight hover:bg-primaryLight text-white"
+      } block transition-all duration-300 rounded-default px-5 py-2 text-lg ${modifyClasses}`}
+    >
+      {text}
+    </button>
+  );
+};
+
+ButtonBtn.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClickFunction: PropTypes.func,
+  outlined: PropTypes.bool,
+  modifyClasses: PropTypes.string,
+};
+
+export default ButtonBtn;
