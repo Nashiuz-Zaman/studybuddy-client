fb786c4859c2a053656c9dd7a99518d5ae47834d
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-07 03:53:52 +0600

    Install React router hash link in the project, create linkBtn component and include it in banner component

--- src/components/shared/Banner/Banner.jsx
+++ src/components/shared/Banner/Banner.jsx
@@ -1,21 +1,30 @@
 // react
 import PropTypes from "prop-types";
 
+// shared component
+import LinkBtn from "./../LinkBtn/LinkBtn";
+
 const Banner = ({ heading = "", imageSource = null }) => {
   return (
     <div>
-      <h1 className="text-center text-2xl xsm:text-3xl sm:text-4xl lg:text-5xl mb-1 font-bold tracking-tight leading-snug">
+      <h1 className="text-center text-2xl xsm:text-3xl sm:text-4xl lg:text-5xl mb-6 font-bold tracking-tight !leading-snug">
         {heading}
       </h1>
-      <div className="w-[80%] lg:max-w-[50rem] mx-auto">
+      <div className="w-[80%] lg:max-w-[50rem] mx-auto mb-10">
         <img className="block w-full" src={imageSource} alt="banner" />
       </div>
+      <LinkBtn
+        hashed={true}
+        url={"#features"}
+        text={"Explore"}
+        modifyClasses="w-[70%] sm:w-[40%] md:w-[20%] text-center rounded-full mx-auto"
+      />
     </div>
   );
 };
 
 Banner.propTypes = {
-  heading: PropTypes.string,
+  heading: PropTypes.node,
   imageSource: PropTypes.string,
 };
 
---
+++ src/components/shared/LinkBtn/LinkBtn.jsx
@@ -0,0 +1,52 @@
+// react imports
+import PropTypes from "prop-types";
+
+// react router imports
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+const LinkBtn = ({
+  text,
+  url,
+  outlined = false,
+  hashed = false,
+  modifyClasses = "",
+}) => {
+  if (hashed) {
+    return (
+      <HashLink
+        className={`${
+          outlined
+            ? "bg-transparent border border-white text-white hover:bg-[rgba(255,255,255,0.5)]"
+            : "bg-primary border border-primary hover:border-primaryLight hover:bg-primaryLight text-white"
+        } block transition-all duration-300 rounded-default px-5 py-2 text-lg ${modifyClasses}`}
+        to={url}
+      >
+        {text}
+      </HashLink>
+    );
+  }
+
+  return (
+    <Link
+      className={`${
+        outlined
+          ? "bg-transparent border border-white text-white hover:bg-[rgba(255,255,255,0.5)]"
+          : "bg-primary border border-primary hover:border-primaryLight hover:bg-primaryLight text-white"
+      } block transition-all duration-300 rounded-default px-5 py-2 text-lg ${modifyClasses}`}
+      to={url}
+    >
+      {text}
+    </Link>
+  );
+};
+
+LinkBtn.propTypes = {
+  text: PropTypes.string,
+  url: PropTypes.string,
+  outlined: PropTypes.bool,
+  hashed: PropTypes.bool,
+  modifyClasses: PropTypes.string,
+};
+
+export default LinkBtn;
