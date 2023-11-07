4e83f8ff8a703843008146ff8edbf2e72917f4fe
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-07 14:54:55 +0600

    Change styles for inner container component

--- src/components/containers/InnerContainer/InnerContainer.jsx
+++ src/components/containers/InnerContainer/InnerContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const InnerContainer = ({ children }) => {
   return (
-    <div className="max-w-[90rem] px-4 md:px-8 lg:px-12 2xl:px-0 mx-auto">
+    <div className="max-w-[90rem] 3xl:max-w-[100rem] px-4 md:px-8 lg:px-12 2xl:px-0 mx-auto">
       {children}
     </div>
   );
