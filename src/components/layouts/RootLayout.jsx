d97a9c9091972417115e03710a677ba38b377a40
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-08 12:55:09 +0600

    Implement logout toast in the root layout

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 
 // shared components
 import Header from "../shared/Header/Header";
+import LogoutToast from "../shared/LogoutToast/LogoutToast";
 
 // data
 import brandLogo from "./../../assets/website-logo/logo.webp";
@@ -11,6 +12,7 @@ function RootLayout() {
   return (
     <div className="font-default text-textPrimary">
       <Header logo={brandLogo} modifyClasses="mb-sectionGapSm" />
+      <LogoutToast />
       <Outlet />
     </div>
   );
