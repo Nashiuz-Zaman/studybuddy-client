151b30ce42f2948e70da8162d5e289b52970f6f5
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-08 13:00:17 +0600

    Change styles of login and registration toasts

--- src/components/shared/RegistratonSuccessToast/RegistrationSuccessToast.jsx
+++ src/components/shared/RegistratonSuccessToast/RegistrationSuccessToast.jsx
@@ -7,7 +7,7 @@ import { IoCheckmarkCircle } from "react-icons/io5";
 const RegistrationSuccessToast = ({ show = false }) => {
   return (
     <div
-      className={`fixed opacity-0 -top-full left-1/2 -translate-x-1/2 flex flex-col items-center gap-6 transition-all duration-150 rounded-default w-max px-8 py-4 shadow-[0_0_30px_rgba(0,0,0,0.2)] bg-white ${
+      className={`fixed opacity-0 -top-full left-1/2 -translate-x-1/2 flex flex-col items-center gap-6 transition-all duration-150 rounded-default w-max px-8 py-4 shadow-[0_0_30px_rgba(0,0,0,0.1)] bg-white ${
         show ? "top-10 opacity-100" : ""
       }`}
     >
